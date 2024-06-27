08124ef4f04633993a33bfa831e4ee278511ed9f
Denilcecaes24 <164238925+Denilcecaes24@users.noreply.github.com>
2024-06-27 15:25:34 -0600

    subiendo archivos

---
+++ js/app-slider-p.js
@@ -0,0 +1,36 @@
+let swiper2 = new Swiper(".container2", {
+    spaceBetween:15,
+    grabCursor:true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true
+    },
+
+    breakpoints: {
+        // Configuración cuando el ancho de la ventana es igual o menor a 568px
+        568: {
+            slidesPerView: 2
+        },
+        // Configuración cuando el ancho de la ventana es igual o menor a 768px
+        // Configuración cuando el ancho de la ventana es igual o menor a 968px
+        768: {
+            slidesPerView: 3
+        },
+        968: {
+            slidesPerView: 4
+        },
+        // Configuración cuando el ancho de la ventana es igual o menor a 968px
+    
+        // Configuración cuando el ancho de la ventana es igual o menor a 1024px
+    
+        // Configuración cuando el ancho de la ventana es igual o menor a 1024px
+     
+    }
+
+    
+})
